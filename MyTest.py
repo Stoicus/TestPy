9846ac75084cfbe4b11cf1633fdf7de56c805006
Stoicus <wgeorg@hotmail.com>
2019-04-28 12:43:04 -0500

    Branch, checkout, charge, commit

--- MyTest.py
+++ MyTest.py
@@ -1,5 +1,22 @@
 #! python3
 
+import re
+
+#   ? 0 or 1
+#   * 0 or more
+#   + 1 or more
+#   . universal character match
+#   \s  whitespace
+#   \S  not whitespace
+#   \d  digit
+#   \D  not digit
+#   \w  word
+#   \W  not word
+#   ^   beginning of line
+#   $   end of line
+#   [0-9]   same as \d
+#   [^0-9]  same as \D
+
 try:
     fhandle = open(r'c:\Users\WGeorg\Downloads\copybook.txt')
 except:
